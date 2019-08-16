eafc2abac0a3cd357900e5478d816a880ceef208
Spike^ekipS <spikeekips@gmail.com>
2019-08-16 16:34:38 +0900

    log file handler improved.

--- contrib/contest/log.go
+++ contrib/contest/log.go
@@ -7,28 +7,38 @@ import (
 	"path/filepath"
 
 	"github.com/inconshreveable/log15"
+	"golang.org/x/sync/syncmap"
 )
 
 var log log15.Logger = log15.New("module", "contest-main")
+
+var logs *syncmap.Map
+
+func init() {
+	logs = &syncmap.Map{}
+}
+
+func openFile(directory, n string) io.Writer {
+	f := filepath.Join(directory, fmt.Sprintf("%s.log", n))
+	l, found := logs.Load(f)
+	if found {
+		return l.(io.Writer)
+	}
+
+	w, err := os.OpenFile(f, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
+	if err != nil {
+		panic(err)
+	}
+
+	logs.Store(f, w)
+	return w
+}
 
 func LogFileByNodeHandler(directory string, fmtr log15.Format, quiet bool) log15.Handler {
 	if _, err := os.Stat(directory); os.IsNotExist(err) {
 		if err := os.MkdirAll(directory, os.ModePerm); err != nil {
 			panic(err)
 		}
-	}
-
-	logs := map[string]*os.File{}
-
-	openFile := func(n string) io.Writer {
-		f := filepath.Join(directory, fmt.Sprintf("%s.log", n))
-		w, err := os.OpenFile(f, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0600)
-		if err != nil {
-			panic(err)
-		}
-
-		logs[n] = w
-		return w
 	}
 
 	findNode := func(c []interface{}) string {
@@ -43,17 +53,13 @@ func LogFileByNodeHandler(directory string, fmtr log15.Format, quiet bool) log15
 		return ""
 	}
 
-	_ = openFile("none")
-
 	h := log15.FuncHandler(func(r *log15.Record) error {
 		var w io.Writer
 		n := findNode(r.Ctx)
 		if len(n) < 1 {
-			w = logs["none"]
-		} else if f, found := logs[n]; found {
-			w = f
+			w = openFile(directory, "none")
 		} else {
-			w = openFile(n)
+			w = openFile(directory, n)
 		}
 
 		_, err := w.Write(fmtr.Format(r))
@@ -69,14 +75,14 @@ func LogFileByNodeHandler(directory string, fmtr log15.Format, quiet bool) log15
 
 type closingHandler struct {
 	log15.Handler
-	files map[string]*os.File
+	files *syncmap.Map
 }
 
 func (h *closingHandler) Close() error {
-	for _, f := range h.files {
-		if err := f.Close(); err != nil {
-			return err
-		}
-	}
+	h.files.Range(func(_, value interface{}) bool {
+		_ = value.(io.Closer).Close()
+		return true
+	})
+
 	return nil
 }

9a79c7ece82315c4cdbc47460d6c06d432678633
Spike^ekipS <spikeekips@gmail.com>
2019-08-27 18:21:33 +0900

    DamagedBallotMaker will interrupt to make intact ballot in contest.

--- contrib/contest/module/block.go
+++ contrib/contest/module/block.go
@@ -1,7 +1,8 @@
-package main
+package contest_module
 
 import (
 	"crypto/rand"
+	mrand "math/rand"
 
 	"github.com/spikeekips/mitum/hash"
 	"github.com/spikeekips/mitum/isaac"
@@ -23,4 +24,21 @@ func NewBlock(height isaac.Height, round isaac.Round) isaac.Block {
 	)
 
 	return bk
+}
+
+func NewRandomBlockHash() hash.Hash {
+	b := make([]byte, 4)
+	_, _ = rand.Read(b)
+
+	h, _ := isaac.NewBlockHash(b)
+
+	return h
+}
+
+func NewRandomHeight() isaac.Height {
+	return isaac.NewBlockHeight(uint64(mrand.Intn(100)))
+}
+
+func NewRandomRound() isaac.Round {
+	return isaac.Round(uint64(mrand.Intn(100)))
 }

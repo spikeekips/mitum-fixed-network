c5483c5ae98dbe106744d193fa4a825f1263a692
Spike^ekipS <spikeekips@gmail.com>
2020-02-12 16:43:46 +0900

    InvalidError defined.

--- isvalid/isvalid.go
+++ isvalid/isvalid.go
@@ -1,5 +1,11 @@
 package isvalid
 
+import "github.com/spikeekips/mitum/errors"
+
+var (
+	InvalidError = errors.NewError("invalid found")
+)
+
 type IsValider interface {
 	IsValid([]byte) error
 }

5b82032882e0f44e430df3febbf7ad92f42cf364
Spike^ekipS <spikeekips@gmail.com>
2020-02-12 15:24:46 +0900

    chained checker added

---
+++ util/checker.go
@@ -0,0 +1,28 @@
+package util
+
+type CheckerFunc func() (bool, error)
+
+type Checker struct {
+	name string
+	fns  []CheckerFunc
+}
+
+func NewChecker(name string, fns []CheckerFunc) Checker {
+	return Checker{
+		name: name,
+		fns:  fns,
+	}
+}
+
+func (ck Checker) Check() error {
+	for _, fn := range ck.fns {
+		keep, err := fn()
+		if err != nil {
+			return err
+		} else if !keep {
+			return nil
+		}
+	}
+
+	return nil
+}
---
+++ util/checker_test.go
@@ -0,0 +1,93 @@
+package util
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+type testChecker struct {
+	suite.Suite
+}
+
+type checkerStruct struct {
+	c0keep    bool
+	c0err     error
+	c1keep    bool
+	c1err     error
+	c2keep    bool
+	c2err     error
+	c0checked bool
+	c1checked bool
+	c2checked bool
+}
+
+func (cs *checkerStruct) Check0() (bool, error) {
+	defer func() { cs.c0checked = true }()
+
+	return cs.c0keep, cs.c0err
+}
+
+func (cs *checkerStruct) Check1() (bool, error) {
+	defer func() { cs.c1checked = true }()
+
+	return cs.c1keep, cs.c1err
+}
+
+func (cs *checkerStruct) Check2() (bool, error) {
+	defer func() { cs.c2checked = true }()
+
+	return cs.c2keep, cs.c2err
+}
+
+func (t *testChecker) TestNew() {
+	cs := &checkerStruct{
+		c0keep: true, c0err: nil,
+		c1keep: true, c1err: nil,
+		c2keep: true, c2err: nil,
+	}
+
+	ck := NewChecker("test", []CheckerFunc{cs.Check0, cs.Check1, cs.Check2})
+
+	t.NoError(ck.Check())
+	t.True(cs.c0checked)
+	t.True(cs.c1checked)
+	t.True(cs.c2checked)
+}
+
+func (t *testChecker) TestKeep() {
+	cs := &checkerStruct{
+		c0keep: true, c0err: nil,
+		c1keep: false, c1err: nil,
+		c2keep: true, c2err: nil,
+	}
+
+	ck := NewChecker("test", []CheckerFunc{cs.Check0, cs.Check1, cs.Check2})
+
+	t.NoError(ck.Check())
+	t.True(cs.c0checked)
+	t.True(cs.c1checked)
+	t.False(cs.c2checked)
+}
+
+func (t *testChecker) TestError() {
+	cs := &checkerStruct{
+		c0keep: true, c0err: nil,
+		c1keep: true, c1err: fmt.Errorf("show me"),
+		c2keep: true, c2err: nil,
+	}
+
+	ck := NewChecker("test", []CheckerFunc{cs.Check0, cs.Check1, cs.Check2})
+
+	err := ck.Check()
+	t.Contains(err.Error(), "show me")
+
+	t.True(cs.c0checked)
+	t.True(cs.c1checked)
+	t.False(cs.c2checked)
+}
+
+func TestChecker(t *testing.T) {
+	suite.Run(t, new(testChecker))
+}

93fbc2dad3e698c4fb2a6aacea3c45529387f6b2
Spike^ekipS <spikeekips@gmail.com>
2020-02-29 21:06:33 +0900

    check code maintenance script added

---
+++ contrib/run-before-commit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+set -x
+
+echo "Running checking"
+
+# go test
+go clean -testcache
+go test -timeout 5s -tags test -race ./... -run .
+
+# go vet
+go vet -tags test ./...
+
+# errcheck
+errcheck -tags test -ignoretests ./...
+
+# golangci-run
+golangci-lint run --build-tags test
+
+exit 0

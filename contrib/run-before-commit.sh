83d0a65ae5a6c38559ea034fc61d17a6856a11a4
Spike^ekipS <spikeekips@gmail.com>
2020-03-10 13:57:02 +0900

    pre-push script updated

--- contrib/run-before-commit.sh
+++ contrib/run-before-commit.sh
@@ -8,22 +8,7 @@ echo "Running checking"
 
 curdir=$(cd $(dirname ${BASH_SOURCE})/..; pwd)
 
-{
-    set +e
-
-    echo 'BLOCK: '
-    output=$(mktemp)
-    go run $curdir/contrib/parse_comment/main.go $curdir 2> /dev/null | grep '\<BLOCK\>'
-    if [ $? -eq 0 ];then
-        echo 'found, exit:'
-        cat $output
-        exit 1
-    else
-        echo 'not found'
-    fi
-
-    set -e
-}
+bash $curdir/contrib/check-BLOCK.sh
 
 echo
 echo 'go test:'

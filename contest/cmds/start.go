b5953630dd8cde9740cf366a053b53e74bb559a9
Spike^ekipS <spikeekips@gmail.com>
2020-05-12 14:36:14 +0900

    contest/main.go will go further

--- contest/cmds/start.go
+++ contest/cmds/start.go
@@ -9,6 +9,23 @@ func (cmd *StartCommand) Run() error {
 	// create docker env
 
 	// generate genesis block and others
+	// BLOCK moves to contest/main.go
+	// BLOCK set basic policy
+	/*{
+		log.Debug().Msg("NodeRunner generated")
+
+		if gg, err := isaac.NewGenesisBlockV0Generator(nr.Localstate(), nil); err != nil {
+			log.Error().Err(err).Msg("failed to create genesis block generator")
+
+			os.Exit(1)
+		} else if blk, err := gg.Generate(); err != nil {
+			log.Error().Err(err).Msg("failed to generate genesis block")
+
+			os.Exit(1)
+		} else {
+			log.Info().Interface("block", blk).Msg("genesis block created")
+		}
+	}*/
 
 	// distribute blocks to other nodes
 

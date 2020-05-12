47e97c06c75a5c19b5ddf27f06fda7ea2bff4eb6
Spike^ekipS <spikeekips@gmail.com>
2020-05-12 14:31:02 +0900

    DecodePrivatekey() added

--- base/key/decode.go
+++ base/key/decode.go
@@ -5,6 +5,18 @@ import (
 	"github.com/spikeekips/mitum/util/hint"
 )
 
+func DecodePrivatekey(enc encoder.Encoder, b []byte) (Privatekey, error) {
+	if i, err := enc.DecodeByHint(b); err != nil {
+		return nil, err
+	} else if i == nil {
+		return nil, nil
+	} else if v, ok := i.(Privatekey); !ok {
+		return nil, hint.InvalidTypeError.Errorf("not key.Privatekey; type=%T", i)
+	} else {
+		return v, nil
+	}
+}
+
 func DecodePublickey(enc encoder.Encoder, b []byte) (Publickey, error) {
 	if i, err := enc.DecodeByHint(b); err != nil {
 		return nil, err

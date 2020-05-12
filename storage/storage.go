c755ffd1b77e7cecff38232a439fb87c0a916fae
Spike^ekipS <spikeekips@gmail.com>
2020-05-12 16:04:35 +0900

    TODO added

--- storage/storage.go
+++ storage/storage.go
@@ -38,6 +38,7 @@ type Storage interface {
 	NewSeals([]seal.Seal) error
 	Seal(valuehash.Hash) (seal.Seal, error)
 	Seals(func(valuehash.Hash, seal.Seal) (bool, error), bool /* sort */, bool /* load Seal? */) error
+	// TODO add get Seals by []valuehash.Hash
 
 	NewProposal(ballot.Proposal) error
 	Proposal(base.Height, base.Round) (ballot.Proposal, error)
